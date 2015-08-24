b3179efea72c2323a9e3fbf2a1504a4772326b4e
Michael Schwarz <michi.schwarz@gmail.com>
2015-08-24 21:38:42 +0200

    Added test code which crashes PyClipper.

---
+++ generator/test-pyclipper-crash.py
@@ -0,0 +1,14 @@
+import pyclipper
+
+pc = pyclipper.Pyclipper()
+
+# Add a single line as the subject.
+pc.AddPath([(-1, -1), (2, 1)], pyclipper.PT_SUBJECT, False)
+
+# Add a square as the clipping region.
+pc.AddPath([(0, 0), (1, 0), (1, 1), (0, 1)], pyclipper.PT_CLIP, True)
+
+# Clip the line using the rectangle.
+solution = pc.Execute(pyclipper.CT_INTERSECTION, pyclipper.PFT_NONZERO, pyclipper.PFT_NONZERO)
+
+print(solution)
