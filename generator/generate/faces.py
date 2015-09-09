526b9e12bb9343f30e07f33fee5e28ec8bf2ecc1
Raoul Bourquin <raoul@ccczh.ch>
2015-09-09 20:57:18 +0200

    Stellation code

--- generator/generate/faces.py
+++ generator/generate/faces.py
@@ -1,5 +1,5 @@
-import sys, math, functools
-from lib import polyhedra, paths, export, util
+import sys, math, functools, numpy, operator
+from lib import polyhedra, paths, linalg, export, util
 
 
 def well_height(thickness, angle):
@@ -20,6 +20,70 @@ def get_inversion_points(length):
 	num_inversions = 2 * math.ceil(length * teeth_per_unit)
 	
 	return [i * length / num_inversions for i in range(1, num_inversions)]
+
+
+def stellation_over_edge(polyview):
+	"""
+	"""
+	k1, k2, n = polyhedra.local_coordinates(polyview)
+	u = polyview.vertex_coordinate
+
+	opposite = polyview.opposite
+	l1, l2, m = polyhedra.local_coordinates(opposite)
+	yield (u, k1, -m)
+
+	for face in opposite.face_cycle:
+		neighbour = face.opposite
+
+		l1, l2, m = polyhedra.local_coordinates(neighbour)
+		v = neighbour.vertex_coordinate
+
+		if linalg.norm(numpy.cross(n, m)) < linalg.parallel_eps:
+			print("Parallel faces detected")
+			continue
+		else:
+			s, r = linalg.intersect((u, k1, k2), (v, l1, l2))
+			yield (s, r, m)
+
+
+def line_to_face_coordinates(view, lines):
+	"""
+	"""
+	# TODO: Factor out common face projection
+	k1, k2, _ = polyhedra.local_coordinates(view)
+	T = numpy.column_stack([k1, k2])
+	P = linalg.projector([k1, k2])
+	d = numpy.dot(P, view.vertex_coordinate).reshape(1,3)
+
+	for line in lines:
+		s, r, n = line
+		sp = numpy.dot(P, s) - d
+		rp = numpy.dot(P, r) # = r
+		np = numpy.dot(P, n)
+
+		At = numpy.dot(numpy.row_stack([sp, rp, np]), T)
+		yield map(lambda v: v.reshape(-1), numpy.vsplit(At,3))
+
+
+def compute_halfplanes(lines):
+	"""
+	"""
+	halfplanes = [paths.half_plane(s, linalg.rot_ccw(n)) for s, r, n in lines]
+	return functools.reduce(operator.__and__, halfplanes)
+
+
+def stellation_over_view(polyview):
+	"""
+	"""
+	cells = []
+
+	for view in polyview.face_cycle:
+		lines = stellation_over_edge(view)
+		lines = line_to_face_coordinates(polyview, lines)
+		cells.append(compute_halfplanes(lines))
+
+	stellation = functools.reduce(operator.__xor__, cells)
+	return stellation
 
 
 def main(src_path):
