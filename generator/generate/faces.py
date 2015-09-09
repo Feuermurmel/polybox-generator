a607f30779f6172c89e03e522f11eb1bc16298e4
Michael Schwarz <michi.schwarz@gmail.com>
2015-09-09 21:06:02 +0200

    Removed some unused and probably useless code.

--- generator/generate/faces.py
+++ generator/generate/faces.py
@@ -2,26 +2,6 @@ import sys, math, functools
 from lib import polyhedra, stellations, paths, export, util
 
 
-def well_height(thickness, angle):
-	if angle < util.tau / 4:
-		return thickness / math.tan(angle)
-	else:
-		return 0
-
-
-def get_inversion_points(length):
-	"""
-	Returns the inversion points for the teeth of an edge of the specified length.
-	
-	This function should return a list of numbers in the interval [0..length] describing at which points the inversion points between wells and teeth lie. Normally, the edge will start with a well at position 0. If It should start with a tooth, 0 needs to be included as an inversion point.
-	"""
-	
-	teeth_per_unit = 3
-	num_inversions = 2 * math.ceil(length * teeth_per_unit)
-
-	return [i * length / num_inversions for i in range(1, num_inversions)]
-
-
 def main(src_path):
 	polyhedron = polyhedra.Polyhedron.load_from_json(src_path)
 
