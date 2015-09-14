cb8d984f617c9e24dfbacd4e34d562157a196523
Raoul Bourquin <raoul@ccczh.ch>
2015-09-14 21:46:04 +0200

    Check convexity script

---
+++ generator/check-convexity.py
@@ -0,0 +1,52 @@
+import os, numpy, enum
+from lib import polyhedra
+
+
+class Orientation(enum.Enum):
+	all_inside = 'all_inside'
+	all_outside = 'all_outside'
+	concave = 'concave'
+
+
+def get_polyhedron_orientation(polyhedron : polyhedra.Polyhedron):
+	has_inside_vertices = False
+	has_outside_vertices = False
+
+	for i in polyhedron.faces:
+		# All possible views of all vertices of this face.
+		vertex_views = { k for j in i.face_cycle for k in j.vertex_cycle }
+		transform_from_face = numpy.linalg.inv(polyhedra.face_coordinate_system(i))
+
+		for j in set(polyhedron.vertices) - vertex_views:
+			_, _, z, _ = numpy.dot(transform_from_face, numpy.hstack([j.vertex_coordinate, 1]))
+
+			if z > 0:
+				has_outside_vertices = True
+			else:
+				has_inside_vertices = True
+
+	if not has_outside_vertices:
+		return Orientation.all_inside
+	elif not has_inside_vertices:
+		return Orientation.all_outside
+	else:
+		return Orientation.concave
+
+
+def main():
+	dir = 'src/polyhedra'
+	dir = '/data/Math/polyhedra'
+
+	for i in os.listdir(dir):
+		if i.endswith('.json'):
+			path = os.path.join(dir, i)
+
+			try:
+				polyhedron = polyhedra.Polyhedron.load_from_json(path)
+			except Exception as e:
+				print('{}: {}'.format(path, repr(e)))
+			else:
+				print(path, get_polyhedron_orientation(polyhedron))
+
+
+main()
