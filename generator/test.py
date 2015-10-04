889ab7684999a933cbd69d7d033b1a95c0922c73
Michael Schwarz <michi.schwarz@gmail.com>
2015-10-04 23:19:51 +0200

    Another, simple test boolean operation.

--- generator/test.py
+++ generator/test.py
@@ -16,6 +16,12 @@ def main():
 	with util.writing_text_file('src/test/b.asy') as file:
 		print('fill({}, red + white);'.format(export.asymptote_expression(paths.scale(20) * (c & functools.reduce(operator.__xor__, h)))), file = file)
 		print('draw({}, 0.1mm + black);'.format(export.asymptote_expression(paths.scale(20) * c)), file = file)
+	
+	with util.writing_text_file('src/test/c.asy') as file:
+		x = paths.half_plane((10, 0), (1, 1)) | paths.half_plane((10, 0), (1, -1))
+		
+		print('fill({}, red + white);'.format(export.asymptote_expression(paths.scale(20) * c & x)), file = file)
+		print('draw({}, 0.1mm + black);'.format(export.asymptote_expression(paths.scale(20) * c)), file = file)
 
 
 main()
