e87db0e6ef9059d5d70ea201b739741cbfc3a099
Michael Schwarz <michi.schwarz@gmail.com>
2015-10-17 12:22:10 +0200

    Moved existing code to new OpenSCAD export API.

--- generator/generate/stellations.py
+++ generator/generate/stellations.py
@@ -1,20 +1,21 @@
-from lib import polyhedra, util
-from ._helpers import write_line, write_call, write_group
+import sys
+from lib import polyhedra, util, export
 
 
 @util.main
 def main(src_path):
 	polyhedron = polyhedra.Polyhedron.load_from_json(src_path)
 	
-	write_line('use <../_util.scad>')
+	file = export.OpenSCADFile(sys.stdout)
+	file.line('use <../_util.scad>')
 	
-	with write_group('render'):
+	with file.group('render'):
 		for i in polyhedron.faces:
-			with write_group('intersection'):
-				with write_group('multmatrix', polyhedra.face_coordinate_system(i)):
-					write_call('half_space')
+			with file.group('intersection'):
+				with file.group('multmatrix', polyhedra.face_coordinate_system(i)):
+					file.call('half_space')
 				
 				for j in i.face_cycle:
-					with write_group('multmatrix', polyhedra.face_coordinate_system(j.opposite)):
-						with write_group('scale', [1, 1, -1]):
-							write_call('half_space')
+					with file.group('multmatrix', polyhedra.face_coordinate_system(j.opposite)):
+						with file.group('scale', [1, 1, -1]):
+							file.call('half_space')
