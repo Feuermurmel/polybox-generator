6bf0277541b1231777328f40cb3d10bd17ee17be
Michael Schwarz <michi.schwarz@gmail.com>
2015-10-18 20:18:27 +0200

    Add vertex labels.

--- generator/generate/faces.py
+++ generator/generate/faces.py
@@ -11,14 +11,18 @@ def arrange_grid(count):
 @util.main
 def main(src_path):
 	file = export.AsymptoteFile(sys.stdout)
-	file.write('access "../_faces.asy" as faces;')
+	file.write('import "../_faces.asy" as _;')
 	
 	polyhedron = polyhedra.Polyhedron.load_from_json(src_path)
 	
 	for face, (c, r) in zip(polyhedron.faces, arrange_grid(len(polyhedron.faces))):
-		center = -numpy.mean(polyhedra.get_planar_polygon(face).paths[0].vertices, 0)
+		polygon = polyhedra.get_planar_polygon(face)
+		centerx, centery = -numpy.mean(polygon.paths[0].vertices, 0)
+		cut = stellations.stellation_over_face(face)
 		
-		polygon = paths.move(*center) * polyhedra.get_planar_polygon(face)
-		cut = paths.move(*center) * stellations.stellation_over_face(face)
-		
-		file.write('faces.face({}, {}, {}, {});', polygon, cut, c, r)
+		with file.transform('shift(({}, {}) * 100mm) * scale(20)', c, r):
+			file.write('transform t = shift(({}, {}) * 1mm);', centerx, centery)
+			file.write('face({}, {}, t);', polygon, cut, c, r)
+			
+			for i, (x, y) in zip(face.face_cycle, polygon.paths[0].vertices):
+				file.write('vertex(({}mm, {}mm), {}, t);', x, y, i.vertex_id)
--- src/_faces.asy
+++ src/_faces.asy
@@ -1,9 +1,12 @@
 real offset = 100mm;
 
-void face(path[] polygon, path[] cut, int column, int row) {
-	transform t = shift(column * offset, -row * offset) * scale(20);
-	
+void face(path[] polygon, path[] cut, transform t) {
 	fill(t * cut, red + white);
 	draw(t * polygon, gray + 0.2mm);
 	draw(t * cut, black + 0.01mm);
+}
+
+void vertex(pair vertex, int index, transform t) {
+	dot(t * vertex, gray + black + 1mm);
+	label((string) index, t * vertex + unit(t * vertex) * .5);
 }
