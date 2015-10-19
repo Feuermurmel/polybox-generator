65159eae2eb8e39c6547aed83ff1ca71f7de4532
Raoul Bourquin <raoul@ccczh.ch>
2015-10-20 00:53:30 +0200

    Add more information to face debug

--- generator/generate/faces.py
+++ generator/generate/faces.py
@@ -23,6 +23,13 @@ def main(src_path):
 		with file.transform('shift(({}, {}) * 100mm) * scale(20)', c, r):
 			file.write('transform t = shift(({}, {}) * 1mm);', centerx, centery)
 			file.write('face({}, {}, t);', polygon, cut, c, r)
-			
+
+			file.write('face_id(({}mm, {}mm), "{}", t);', -centerx, -centery, face.face_id)
+
 			for i, (x, y) in zip(face.face_cycle, polygon.paths[0].vertices):
-				file.write('vertex(({}mm, {}mm), {}, t);', x, y, i.vertex_id)
+				file.write('vertex_id(({}mm, {}mm), "{}", t);', x, y, i.vertex_id)
+
+			for i, (ax, ay), (bx, by) in zip(face.face_cycle,
+							 polygon.paths[0].vertices,
+							 polygon.paths[0].vertices[1:] + [polygon.paths[0].vertices[0]]):
+				file.write('edge_id(({}mm, {}mm), ({}mm, {}mm), "{}", t);', ax, ay, bx, by, i.edge_id)
--- src/_faces.asy
+++ src/_faces.asy
@@ -6,7 +6,19 @@ void face(path[] polygon, path[] cut, transform t) {
 	draw(t * cut, black + 0.01mm);
 }
 
-void vertex(pair vertex, int index, transform t) {
+void face_id(pair center, string index, transform t) {
+	label(index, t * center);
+}
+
+void edge_id(pair a, pair b, string index, transform t) {
+	pair edge_center = t * ((a + b) / 2.0);
+	dot(edge_center, gray + black + 1mm);
+	path p = edge_center + unit(edge_center) * 0.5;
+	Label L = scale(0.5) * Label(index, edge_center);
+	label(L, p);
+}
+
+void vertex_id(pair vertex, string index, transform t) {
 	dot(t * vertex, gray + black + 1mm);
-	label((string) index, t * vertex + unit(t * vertex) * .5);
+	label(index, t * vertex + unit(t * vertex) * 0.5);
 }
