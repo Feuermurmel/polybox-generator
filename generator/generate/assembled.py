adab1957ffd934978acec7f9d25552fe1ff375e9
Raoul Bourquin <raoul@ccczh.ch>
2015-10-21 00:46:42 +0200

    Engrave faces

--- generator/generate/assembled.py
+++ generator/generate/assembled.py
@@ -1,11 +1,6 @@
-import sys, math, numpy
-from lib import polyhedra, stellations, paths, export, util
-
-
-def arrange_grid(count):
-	width = math.ceil(math.sqrt(count))
-	
-	return [divmod(i, width) for i in range(count)]
+import sys
+import numpy, numpy.linalg
+from lib import polyhedra, stellations, export, util
 
 
 @util.main
@@ -20,8 +15,18 @@ def main(src_path):
 		for face in polyhedron.faces:
 			cut = stellations.stellation_over_face(face)
 			t = polyhedra.face_coordinate_system(face)
-			
+
+			polygon = polyhedra.get_planar_polygon(face)
+			vertices = polygon.paths[0].vertices
+			centerx, centery = -numpy.mean(vertices, 0)
+			maxr = numpy.amax([numpy.linalg.norm(v) for v in vertices])
+
 			with file.group('multmatrix', t):
-				with file.group('linear_extrude', thickness - gap):
-					with file.group('offset', -gap / 2):
-						file.polygon(cut)
+				with file.group('difference', None):
+					with file.group('linear_extrude', thickness - gap):
+						with file.group('offset', -gap / 2):
+							file.polygon(cut)
+
+					with file.group('translate', [-centerx, -centery, 0.7 * thickness]):
+						with file.group('linear_extrude', thickness):
+							file.text(str(face.face_id), size=0.2 * maxr, halign="center", valign="center")
