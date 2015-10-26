28c862107540a85198b8e02d5ba3afb51a5f4f91
Raoul Bourquin <raoul@ccczh.ch>
2015-10-26 23:47:07 +0100

    Introduce debug mode

--- generator/generate/faces.py
+++ generator/generate/faces.py
@@ -15,6 +15,8 @@ def main(src_path):
 
 	polyhedron = polyhedra.Polyhedron.load_from_json(src_path, scale=1)
 
+	debug_mode = True
+
 	for face, (c, r) in zip(polyhedron.faces, arrange_grid(len(polyhedron.faces))):
 		polygon = polyhedra.get_planar_polygon(face)
 		centerx, centery = numpy.mean(polygon.paths[0].vertices, 0)
@@ -22,18 +24,21 @@ def main(src_path):
 		
 		with file.transform('shift(({}, {}) * 100mm) * scale(20)', c, r):
 			file.write('transform t = shift(({}, {}) * 1mm);', -centerx, -centery)
+
+			if debug_mode:
+				file.write('face({}, t);', cut, c, r)
+				file.write('edges({}, t);', polygon, c, r)
+				file.write('vertices({}, t);', polygon, c, r)
+
+				file.write('face_id(({}mm, {}mm), "{}", t);', centerx, centery, face.face_id)
+
+				for i, (x, y) in zip(face.face_cycle, polygon.paths[0].vertices):
+					file.write('vertex_id(({}mm, {}mm), "{}", t);', x, y, i.vertex_id)
+
+					for i, (ax, ay), (bx, by) in zip(face.face_cycle,
+									 polygon.paths[0].vertices,
+									 polygon.paths[0].vertices[1:] + [polygon.paths[0].vertices[0]]):
+						file.write('edge_id(({}mm, {}mm), ({}mm, {}mm), "{}", t);', ax, ay, bx, by, i.edge_id)
+
+			# Contour for laser cut
 			file.write('cut_contour({}, t);', cut, c, r)
-
-			file.write('face({}, t);', cut, c, r)
-			file.write('edges({}, t);', polygon, c, r)
-			file.write('vertices({}, t);', polygon, c, r)
-
-			file.write('face_id(({}mm, {}mm), "{}", t);', centerx, centery, face.face_id)
-
-			for i, (x, y) in zip(face.face_cycle, polygon.paths[0].vertices):
-				file.write('vertex_id(({}mm, {}mm), "{}", t);', x, y, i.vertex_id)
-
-			for i, (ax, ay), (bx, by) in zip(face.face_cycle,
-							 polygon.paths[0].vertices,
-							 polygon.paths[0].vertices[1:] + [polygon.paths[0].vertices[0]]):
-				file.write('edge_id(({}mm, {}mm), ({}mm, {}mm), "{}", t);', ax, ay, bx, by, i.edge_id)
